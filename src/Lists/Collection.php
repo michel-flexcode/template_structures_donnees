a077c3262427abe4691b56a50279f21bc3dbf796
MichelCode <michelseocode.com>
2023-10-23 15:21:27 +0200

    save Monday

--- src/Lists/Collection.php
+++ src/Lists/Collection.php
@@ -6,29 +6,117 @@ namespace Opmvpc\StructuresDonnees\Lists;
 
 class Collection extends ArrayList implements CollectionInterface
 {
-    public function map(callable $callback): CollectionInterface {}
+    public function map(callable $callback): CollectionInterface
+    {
+        $collection = new Collection();
 
-    public function filter(callable $callback): CollectionInterface {}
+        foreach ($this->toArray() as $key => $value) {
+            $collection->push($callback($value, $key));
+        }
 
-    public function reduce(callable $callback, mixed $initial = null): mixed {}
+        return $collection;
+    }
 
-    public function forEach(callable $callback): void {}
+    public function filter(callable $callback): CollectionInterface
+    {
+        //Stock est de classe collection 
+        $Stock = new Collection();
 
-    public function some(callable $callback): bool {}
+        foreach ($this->toArray() as $key => $value) {
+            if ($callback($value, $key)) { // Use the callback to determine if the value should be included
+                $Stock->push($value);
+            }
+        }
 
-    public function every(callable $callback): bool {}
+        return $Stock;
+    }
 
-    public function find(callable $callback): mixed {}
+    public function reduce(callable $callback, mixed $initial = null): mixed
+    {
+        $carry = $initial;
 
-    public function findIndex(callable $callback): int {}
+        foreach ($this->toArray() as $key => $value) {
+            //$key utile si besoin d'un affichage spécifique, facultatif
+            $carry = $callback($carry, $value, $key);
+        }
 
-    public function join(string $separator = ', '): string {}
+        return $carry;
+    }
 
-    public function reverse(): CollectionInterface {}
+    public function forEach(callable $callback): void
+    {
+        $summ = 0;
 
-    public function sort(callable $callback = null): CollectionInterface {}
+        foreach ($this->toArray() as $key => $value) {
+            //$key utile si besoin d'un affichage spécifique, facultatif
+            $summ = $callback($value);
+        }
+    }
 
-    public function concat(...$collections): CollectionInterface {}
+    public function some(callable $callback): bool
+    {
+        foreach ($this->toArray() as $key => $value) {
+            if ($callback($value, $key)) {
+                return true; // Retourne true dès qu'un élément satisfait la condition.
+            }
+        }
 
-    public function fill(mixed $value = null, int $start = 0, ?int $end = null): CollectionInterface {}
+        return false; // Si aucun élément ne satisfait la condition, retourne false.
+    }
+
+    public function every(callable $callback): bool
+    {
+        foreach ($this->toArray() as $key => $value) {
+            if ($callback($value, $key)) {
+                return true; // Retourne true dès qu'un élément satisfait la condition.
+            }
+        }
+
+        return false; // Si aucun élément ne satisfait la condition, retourne false.
+    }
+
+    public function find(callable $callback): mixed
+    {
+        foreach ($this->toArray() as $key => $value) {
+            if ($callback($value, $key)) {
+                return $value; // Renvoie la première correspondance trouvée
+            }
+        }
+
+        return null; // Renvoie null si aucune correspondance n'a été trouvée
+    }
+
+    public function findIndex(callable $callback): int
+    {
+    }
+
+    public function join(string $separator = ', '): string
+    {
+    }
+
+    public function reverse(): CollectionInterface
+    {
+        $collection = new Collection();
+
+        $array = $this->toArray();
+        $reversedArray = array_reverse($array);
+
+        foreach ($reversedArray as $value) {
+            $collection->push($value);
+        }
+
+        return $collection;
+    }
+
+    public function sort(callable $callback = null): CollectionInterface
+    {
+    }
+
+    public function concat(...$collections): CollectionInterface
+    {
+    }
+
+    public function fill(mixed $value = null, int $start = 0, ?int $end = null): CollectionInterface
+    {
+    }
 }
